af8ad496fdfee3ce85a280dc19fac4f6ebbde7aa
benjaminbriolat <briolatbenjamin@gmail.com>
2024-04-18 12:01:22 +0200

    test modif script bool

--- testGit2/Assets/Scripts/TestScriptGit.cs
+++ testGit2/Assets/Scripts/TestScriptGit.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TestScriptGit : MonoBehaviour
 {
     string TestString = "TestGit";
+    bool testBool = false;
     // Start is called before the first frame update
     void Start()
     {
