64f59c950351791413f4f39ef082fc39c74eec53
philippeduhamel11 <philippeduhamel11@gmail.com>
2024-04-18 11:58:35 +0200

    Test Script Commit
    ++ Test Script

---
+++ testGit2/Assets/Scripts/TestScriptGit.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScriptGit : MonoBehaviour
+{
+    string TestString = "TestGit";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
